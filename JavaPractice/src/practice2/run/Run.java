d3f9ef27c31255ed179f30edfc9492a13436780c
BaekSeongJun <por9535@gmail.com>
2026-06-12 10:39:39 +0900

    2026-06-12 mvc practice2 마무리

--- JavaPractice/src/practice2/run/Run.java
+++ JavaPractice/src/practice2/run/Run.java
@@ -1,7 +1,49 @@
 package practice2.run;
 
+import java.util.Scanner;
+
+import practice2.view.BookMenu;
+
 public class Run {
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
+		boolean exitFlag = false;
+		while(!exitFlag) {
+			System.out.println("1.도서, 2.판매, 3.회원관리, 4.관리자, 5.게시판 ,6.종료");
+			System.out.print("1~6번선택 >>");
+			int no = 0;
+			try{
+				no = Integer.parseInt(sc.nextLine());
+			}catch(Exception e){
+				System.out.println("예상치 못한 오류가 발생했습니다. 다시 시도해주세요");
+			}
+
+			switch (no) {
+				case 1: {
+					BookMenu bm = new BookMenu();
+					bm.bookInfoDisplay();
+					break;
+				}
+				case 2:{
+					System.out.println("도서판매는 작업중");
+					break;
+				}
+				case 6:{
+					System.out.println("프로그램을 종료합니다.");
+					exitFlag = true;
+					break;
+				}
+				default:
+					System.out.println("고객님 메뉴에 있는 번호선택해주세요.");
+					break;
+			}
+		}
+		try {
+			if (sc != null) {
+				sc.close();
+			}
+		}catch (Exception e) {
+		}
 
 	}
 }
--- JavaPractice/src/practice2/view/BookMenu.java
+++ JavaPractice/src/practice2/view/BookMenu.java
@@ -2,6 +2,7 @@ package practice2.view;
 
 import practice2.controller.BookController;
 import practice2.model.Book;
+import practice2.run.Run;
 
 public class BookMenu {
 	private BookController bc;
@@ -9,16 +10,67 @@ public class BookMenu {
 		bc = new BookController();
 	}
 
+	public void bookInfoDisplay(){
+		boolean flag = false;
+		while(!flag) {
+			System.out.println("1.책 정보 출력 | 2.책 정렬(대여순) | 3.총 대여수 출력 | 4. 대여수와 평균 출력 | 5. HOME ");
+			System.out.print("번호 입력 : ");
+			int no = 0;
+			try {
+				no = Integer.parseInt(Run.sc.nextLine());
+			} catch (Exception e) {
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요");
+				continue;
+			}
+			switch (no) {
+				case 1:
+					printBooks();
+					break;
+				case 2:
+					printSortedBooks();
+					break;
+				case 3:
+					getRentSum();
+					break;
+				case 4:
+					getRentSumAvg();
+					break;
+				case 5:
+					System.out.println("Home으로 돌아갑니다.");
+					flag = true;
+					break;
+				default:
+					System.out.println("잘못된 입력입니다. 다시 입력해주세요");
+					break;
+			}
+		}
+	}
+
 	//첫번째 책 정보를 다보여주세요. 요청하는 화면
 	public void printBooks(){
+		System.out.println("========== 도서 정보 출력 ==========");
 		Book[] books = bc.printBook();
-		for(Book b : books){
-			System.out.println(b.inform());
+		for(Book book : books){
+			System.out.println(book.inform());
 		}
 	}
 	//두번째 책 정보를 대여순으로 정렬해서 보여주세요.
-
+	private void printSortedBooks(){
+		System.out.println("========== 도서 정보 정렬 ==========");
+		Book[] books = bc.sortByRentCountDesc();
+		for (Book book : books) {
+			System.out.println(book.inform());
+		}
+	}
 	//세번째 책 정보에서 총 대여수를 출력해주세요.
-
+	public void getRentSum(){
+		System.out.println("========== 총 대여수 출력 ==========");
+		System.out.printf("총 대여수 : %d\n", bc.sumRentCount());
+	}
 	//네번째 책 정보에서 총 대여수와 총대여수의 평균값을 출력해주세요.
+	public void getRentSumAvg(){
+		System.out.println("========== 대여 통계 출력 ==========");
+		double[] sumAvg = bc.avgRentCount();
+		System.out.printf("총 대여수 : %d\n 총 대여 평균 : %5.2f\n", (int)sumAvg[0], sumAvg[1]);
+	}
 }
